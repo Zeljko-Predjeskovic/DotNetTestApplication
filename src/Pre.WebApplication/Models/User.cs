cd8b3ac384366633b05c7fc6b53d72ea11ac6a0d
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-09-04 19:14:41 +0200

    Tested razor component

---
+++ src/Pre.WebApplication/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pre.WebApplication.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int age { get; set; }
+
+    }
+}
